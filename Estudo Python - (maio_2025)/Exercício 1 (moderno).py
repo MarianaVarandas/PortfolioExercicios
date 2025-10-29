c7d2e70660d14608eaf477a94dde8f55ff33d18d
Mariana (Dayalu) Varandas <mariana.varandas1@gmail.com>
2025-10-29 11:54:14 -0300

    resolvendo problema de rastreamento pasta Python

---
+++ Estudo Python - (maio_2025)/Exercício 1 (moderno).py
@@ -0,0 +1,6 @@
+x = int(input("digite um número inteiro: "))
+y = int(input("digite outro número inteiro: "))
+
+res = "o resultado da soma de {} commais {} é igual a {}".format (x, y, x+y)
+
+print(res)
---
+++ Estudo Python - (maio_2025)/Exercício 4 (variável fatiada).py
@@ -0,0 +1,5 @@
+frase = input ("escreva uma frase: ")
+tam = len(frase)
+metade_frase = frase[:int(tam/2)]
+
+print(metade_frase[-2:])
