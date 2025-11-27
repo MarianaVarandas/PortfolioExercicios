56cc12553cccea95915349f62925353c31478984
Mariana (Dayalu) Varandas <mariana.varandas1@gmail.com>
2025-11-27 11:05:48 -0300

    Inserindo mais exercícios

---
+++ Estudo Java - (mar_2025, out2025)/ArrayLists_castings_instanceof/Classes/ContaBancaria.java
@@ -0,0 +1,19 @@
+package ArrayLists_castings_instanceof.Classes;
+
+public class ContaBancaria {
+    private int conta;
+    private double saldo;
+
+    public ContaBancaria(int conta, double saldo) {
+        this.conta = conta;
+        this.saldo = saldo;
+    }
+
+    public int getConta() {
+        return conta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
---
+++ Estudo Java - (mar_2025, out2025)/ArrayLists_castings_instanceof/Interface/Forma.java
@@ -0,0 +1,5 @@
+package ArrayLists_castings_instanceof.Interface;
+
+public interface Forma {
+    public double calculaArea();
+}
